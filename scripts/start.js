4870235970e8e1bba09c0c7763f85cd43127b03d
Romez <Romez1990@gmail.com>
2021-01-09 14:32:36 +0300

    feat(scripts): replace build with watch in start script

--- scripts/start.js
+++ scripts/start.js
@@ -16,27 +16,16 @@ process.on('unhandledRejection', err => {
 // Ensure environment variables are read.
 require('../config/env');
 
-const path = require('path');
 const chalk = require('react-dev-utils/chalk');
 const fs = require('fs-extra');
-const bfj = require('bfj');
 const webpack = require('webpack');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
-const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
 const printBuildError = require('react-dev-utils/printBuildError');
 const { checkBrowsers } = require('react-dev-utils/browsersHelper');
 const paths = require('../config/paths');
 const configFactory = require('../config/webpack.config');
 const { checkWarnings, copyPublicFolder } = require('./common');
-
-const { printFileSizesAfterBuild } = FileSizeReporter;
-const useYarn = fs.existsSync(paths.yarnLockFile);
-
-// These sizes are pretty large. We'll warn for bundles exceeding them.
-const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
-const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
 
 const isInteractive = process.stdout.isTTY;
 
@@ -44,9 +33,6 @@ const isInteractive = process.stdout.isTTY;
 if (!checkRequiredFiles([paths.appHtml, paths.appMainJs])) {
   process.exit(1);
 }
-
-const argv = process.argv.slice(2);
-const writeStatsJson = argv.indexOf('--stats') !== -1;
 
 // Generate configuration
 const config = configFactory('development');
@@ -62,12 +48,14 @@ checkBrowsers(paths.appPath, isInteractive)
     copyPublicFolder();
     // Start the webpack build
     const compiler = webpack(config);
-    return new Promise((resolve, reject) => {
-      compiler.run((err, stats) => {
+    compiler.watch(
+      config.watchOptions,
+      (err, stats) => {
         let messages;
         if (err) {
           if (!err.message) {
-            return reject(err);
+            handleError(err);
+            return;
           }
 
           let errMessage = err.message;
@@ -90,7 +78,8 @@ checkBrowsers(paths.appPath, isInteractive)
           if (messages.errors.length > 1) {
             messages.errors.length = 1;
           }
-          return reject(new Error(messages.errors.join('\n\n')));
+          handleError(new Error(messages.errors.join('\n\n')));
+          return;
         }
         if (
           process.env.CI &&
@@ -103,65 +92,34 @@ checkBrowsers(paths.appPath, isInteractive)
               'Most CI servers set it automatically.\n',
             ),
           );
-          return reject(new Error(messages.warnings.join('\n\n')));
+          handleError(new Error(messages.warnings.join('\n\n')));
         }
 
-        const resolveArgs = {
+        checkWarnings({
           stats,
-          previousFileSizes,
           warnings: messages.warnings,
-        };
-
-        if (writeStatsJson) {
-          return bfj
-            .write(`${paths.appBuild}/bundle-stats.json`, stats.toJson())
-            .then(() => resolve(resolveArgs))
-            .catch(error => reject(new Error(error)));
-        }
-
-        return resolve(resolveArgs);
-      });
-    });
+        });
+      },
+    );
   })
-  .then(
-    ({ stats, previousFileSizes, warnings }) => {
-      checkWarnings(warnings);
-
-      console.log('File sizes after gzip:\n');
-      printFileSizesAfterBuild(
-        stats,
-        previousFileSizes,
-        paths.appBuild,
-        WARN_AFTER_BUNDLE_GZIP_SIZE,
-        WARN_AFTER_CHUNK_GZIP_SIZE,
-      );
-      console.log();
-
-      const appPackage = require(paths.appPackageJson);
-      const publicUrl = paths.publicUrlOrPath;
-      const { publicPath } = config.output;
-      const buildFolder = path.relative(process.cwd(), paths.appBuild);
-      printHostingInstructions(appPackage, publicUrl, publicPath, buildFolder, useYarn);
-    },
-    err => {
-      const tscCompileOnError = process.env.TSC_COMPILE_ON_ERROR === 'true';
-      if (tscCompileOnError) {
-        console.log(
-          chalk.yellow(
-            'Compiled with the following type errors (you may want to check these before deploying your app):\n',
-          ),
-        );
-        printBuildError(err);
-      } else {
-        console.log(chalk.red('Failed to compile.\n'));
-        printBuildError(err);
-        process.exit(1);
-      }
-    },
-  )
   .catch(err => {
     if (err && err.message) {
       console.log(err.message);
     }
     process.exit(1);
   });
+
+function handleError(err) {
+  const tscCompileOnError = process.env.TSC_COMPILE_ON_ERROR === 'true';
+  if (tscCompileOnError) {
+    console.log(
+      chalk.yellow(
+        'Compiled with the following type errors (you may want to check these before deploying your app):\n',
+      ),
+    );
+    printBuildError(err);
+  } else {
+    console.log(chalk.red('Failed to compile.\n'));
+    printBuildError(err);
+  }
+}
