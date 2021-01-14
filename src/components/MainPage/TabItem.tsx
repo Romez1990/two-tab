2b881c388a77f8bff9b4ee00257498a819771980
Romez <Romez1990@gmail.com>
2021-01-14 21:10:32 +0300

    temp

--- src/components/MainPage/TabItem.tsx
+++ src/components/MainPage/TabItem.tsx
@@ -1,13 +1,22 @@
 import React, { FC } from 'react';
 import { ListItem } from '@material-ui/core';
 import { Tab } from '../../services/TabList';
+import { useService } from '../ServiceContainer';
 
 interface Props {
   readonly tab: Tab;
 }
 
-export const TabItem: FC<Props> = ({ tab }) => (
-  <ListItem>
-    <div>{tab.title}</div>
-  </ListItem>
-);
+export const TabItem: FC<Props> = ({ tab }) => {
+  const urlProcessing = useService('urlProcessingService');
+
+  const hostName = urlProcessing.getHostName(tab.url);
+
+  return (
+    <ListItem>
+      <a href={tab.url} target="_blank" rel="noreferrer">
+        {tab.title} <span>{hostName}</span>
+      </a>
+    </ListItem>
+  );
+};
