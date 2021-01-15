08078f5b0b228b55395cfc763830198f5b6b5cc0
Romez <Romez1990@gmail.com>
2021-01-15 11:07:39 +0300

    temp

--- src/services/Serializer/JsonSerializerImpl.ts
+++ src/services/Serializer/JsonSerializerImpl.ts
@@ -4,6 +4,7 @@ export class JsonSerializerImpl implements JsonSerializer {
   public constructor(private readonly json: JSON) {}
 
   public serialize(data: unknown, pretty?: boolean): string {
+    if (typeof data === 'undefined') return 'undefined';
     if (pretty) {
       return this.json.stringify(data, null, 2);
     }
