620237b4a800ee61dfbe961220514304a2cc3df9
Romez <Romez1990@gmail.com>
2021-01-19 12:16:10 +0300

    temp

--- src/services/Utils/fp-ts/Task.ts
+++ src/services/Utils/fp-ts/Task.ts
@@ -1,14 +1,3 @@
 import { Task } from 'fp-ts/Task';
 
 export const run = <T>(task: Task<T>): Promise<T> => task();
-
-export async function runWithErrorThrowing<T>(task: Task<T>): Promise<T> {
-  try {
-    return await run(task);
-  } catch (error) {
-    setTimeout(() => {
-      throw error;
-    });
-    throw error;
-  }
-}
