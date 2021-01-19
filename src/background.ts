aa7b5b9daafea4694e3b8fcc4df8b00b2fb0588d
Romez <Romez1990@gmail.com>
2021-01-19 17:16:25 +0300

    temp

--- src/background.ts
+++ src/background.ts
@@ -2,12 +2,12 @@ import { pipe } from 'fp-ts/function';
 import { serviceContainer } from './services/ServiceContainer';
 import { run } from './services/Utils/fp-ts/Task';
 
-const loggerService = serviceContainer.get('loggerService');
-const storageService = serviceContainer.get('storageService');
+const logger = serviceContainer.get('loggerService');
+const storage = serviceContainer.get('storageService');
 
 pipe(
-  loggerService.registerReceiver(),
-  storageService.connect.bind(storageService),
+  logger.registerReceiver(),
+  storage.connect.bind(storage),
   run,
   //
 );
