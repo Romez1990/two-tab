08a2e2417713536be09ae3269a87fb680258c528
Romez <Romez1990@gmail.com>
2021-01-22 15:25:08 +0300

    rename popup

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Providers } from './components/Providers';
-import { PopupPanel } from './components/PopupPanel';
+import { PopupPanel } from './components/Popup';
 
 export const Popup: FC = () => (
   <Providers>
