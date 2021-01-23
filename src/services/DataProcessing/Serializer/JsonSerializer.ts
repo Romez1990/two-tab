b7a316d9a5b5cfc52d5c98e59d1956756cab4fb6
Romez <Romez1990@gmail.com>
2021-01-23 08:30:10 +0300

    temp

--- src/services/DataProcessing/Serializer/JsonSerializer.ts
+++ src/services/DataProcessing/Serializer/JsonSerializer.ts
@@ -1,6 +1,4 @@
-import { Type } from 'io-ts';
-
 export interface JsonSerializer {
   serialize(object: unknown, pretty?: boolean): string;
-  deserialize<A, O = A, I = unknown>(json: string, type: Type<A, O, I>): A;
+  deserialize(json: string): unknown;
 }
--- src/services/DataProcessing/Serializer/JsonSerializerImpl.ts
+++ src/services/DataProcessing/Serializer/JsonSerializerImpl.ts
@@ -1,23 +1,13 @@
-import { pipe } from 'fp-ts/function';
-import { Type } from 'io-ts';
-import { TypeCheckingService } from '../TypeChecking';
 import { JsonSerializer } from './JsonSerializer';
 
 export class JsonSerializerImpl implements JsonSerializer {
-  public constructor(private readonly json: JSON, private readonly typeChecking: TypeCheckingService) {}
+  public constructor(private readonly json: JSON) {}
 
   public serialize(data: unknown, pretty?: boolean): string {
     if (typeof data === 'undefined') return 'undefined';
-    if (pretty) {
-      return this.json.stringify(data, null, 2);
-    }
-    return this.json.stringify(data);
+    const spaces = pretty ? 2 : undefined;
+    return this.json.stringify(data, null, spaces);
   }
 
-  public deserialize = <A, O = A, I = unknown>(json: string, type: Type<A, O, I>): A =>
-    pipe(
-      this.json.parse(json),
-      this.typeChecking.checkAndThrow(type),
-      //
-    );
+  public deserialize = (json: string): unknown => this.json.parse(json);
 }
