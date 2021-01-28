215fa8f774ac4bc587413566e105840be22d7322
Romez <Romez1990@gmail.com>
2021-01-28 21:04:47 +0300

    temp

--- src/services/Infrastructure/Config/Types.ts
+++ src/services/Infrastructure/Config/Types.ts
@@ -1,5 +1,3 @@
-import { literal, TypeOf, union } from 'io-ts';
+import { literal, union } from 'io-ts';
 
 export const EnvironmentT = union([literal('development'), literal('production'), literal('test')]);
-
-export type Environment = TypeOf<typeof EnvironmentT>;
