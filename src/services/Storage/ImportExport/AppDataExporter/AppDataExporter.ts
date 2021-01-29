f49c239b9f443ccd205efe16aa471b4e4b9134df
Romez <Romez1990@gmail.com>
2021-01-29 21:17:20 +0300

    temp

--- src/services/Storage/ImportExport/AppDataExporter/AppDataExporter.ts
+++ src/services/Storage/ImportExport/AppDataExporter/AppDataExporter.ts
@@ -4,13 +4,13 @@ import { StoredTab } from '../../TabList/StoredTab';
 import { StoredTabListToCreate } from '../../TabList/StoredTabListToCreate';
 import { StoredTabToCreate } from '../../TabList/StoredTabToCreate';
 import { DataExporter } from '../DataExporter';
-import { ExportedData, ExportedDataOutput, ExportedDataT } from './Data';
+import { ExportedData, ExportedDataOutput, AppExportedDataT } from './AppExportedData';
 import { TabListExportSerializer } from './TabListExportSerializer';
 
 export class AppDataExporter implements DataExporter<ExportedData, ExportedDataOutput> {
   public constructor(private readonly tabListExportSerializer: TabListExportSerializer) {}
 
-  public readonly exportedDataType: Type<ExportedData, ExportedDataOutput> = ExportedDataT;
+  public readonly exportedDataType: Type<ExportedData, ExportedDataOutput> = AppExportedDataT;
 
   public serialize = (
     storedTabLists: ReadonlyArray<StoredTabList>,
