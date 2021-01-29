9a161d4f4e92d2771fa3b55cb6f08dabc967c3d8
Romez <Romez1990@gmail.com>
2021-01-29 13:27:13 +0300

    temp

--- src/services/DOM/Download/DownloadServiceImpl.ts
+++ src/services/DOM/Download/DownloadServiceImpl.ts
@@ -9,13 +9,19 @@ export class DownloadServiceImpl implements DownloadService {
       [content],
       blobData => new Blob(blobData, { type: 'application/octet-stream' }),
       URL.createObjectURL,
-      blobURL => {
-        const link = this.document.createElement('a');
-        link.style.display = 'none';
-        link.href = blobURL;
-        link.setAttribute('download', filename);
-        link.click();
-        URL.revokeObjectURL(blobURL);
-      },
+      blobURL =>
+        pipe(
+          this.createLink(filename, blobURL),
+          link => link.click(),
+          () => URL.revokeObjectURL(blobURL),
+        ),
     );
+
+  private createLink(filename: string, blobURL: string): HTMLAnchorElement {
+    const link = this.document.createElement('a');
+    link.style.display = 'none';
+    link.href = blobURL;
+    link.setAttribute('download', filename);
+    return link;
+  }
 }
