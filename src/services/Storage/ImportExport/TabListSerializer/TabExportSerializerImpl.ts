a066c6d8ba8bf37ecc1b96952224c30da74bb825
Romez <Romez1990@gmail.com>
2021-01-29 08:13:56 +0300

    temp

--- src/services/Storage/ImportExport/TabListSerializer/TabExportSerializerImpl.ts
+++ src/services/Storage/ImportExport/TabListSerializer/TabExportSerializerImpl.ts
@@ -5,7 +5,7 @@ import { StoredTabList } from '../../TabList/StoredTabList';
 import { StoredTabToCreate } from '../../TabList/StoredTabToCreate';
 
 export class TabExportSerializerImpl implements TabExportSerializer {
-  public serialize = ({ id, ...serializedTab }: StoredTab): ExportSerializedTab => serializedTab;
+  public serialize = ({ id, tabListId, ...serializedTab }: StoredTab): ExportSerializedTab => serializedTab;
 
   public deserialize = ({ id }: StoredTabList) => (serializedTab: ExportSerializedTab): StoredTabToCreate => ({
     ...serializedTab,
