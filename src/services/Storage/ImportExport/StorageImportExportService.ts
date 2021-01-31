4be6d70b9103de7055c78e9b87a30a30dc82cdf1
Romez <Romez1990@gmail.com>
2021-01-31 10:23:18 +0300

    temp

--- src/services/Storage/ImportExport/StorageImportExportService.ts
+++ src/services/Storage/ImportExport/StorageImportExportService.ts
@@ -1,8 +1,10 @@
 import { Task } from 'fp-ts/Task';
 import { TaskEither } from 'fp-ts/TaskEither';
 import { TypeCheckingError } from '../../DataProcessing/TypeChecking';
+import { ExportStrategyName } from './exportStrategies';
 
 export interface StorageImportExportService {
+  setStrategy(strategyName: ExportStrategyName): void;
   export(): Task<string>;
   import(json: string): TaskEither<TypeCheckingError, void>;
 }
