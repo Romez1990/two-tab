23b4a44216c8333ac7051b63aa9a5fb83adf4748
Romez <Romez1990@gmail.com>
2021-02-07 10:57:17 +0300

    temp

--- src/services/Infrastructure/Error/AppError.ts
+++ src/services/Infrastructure/Error/AppError.ts
@@ -1,5 +1,5 @@
 export abstract class AppError extends Error {
-  protected constructor(message: string) {
+  public constructor(message: string) {
     super(message);
     this.name = this.constructor.name;
   }
