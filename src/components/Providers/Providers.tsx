b4b04e7359de8efc68f560b3263c9681e725c4fc
Romez <Romez1990@gmail.com>
2021-02-08 21:23:42 +0300

    temp

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react';
 import { ErrorBoundary } from './ErrorBoundary';
 import { ServiceContainerProvider } from './ServiceContainer';
 import { ThemeProvider } from './ThemeProvider';
-import { StorageConnector } from './Storage';
 import { ServiceContainer } from '../../services/Infrastructure/ServiceContainer';
 
 interface Props {
@@ -12,9 +11,7 @@ interface Props {
 export const Providers: FC<Props> = ({ serviceContainer, children }) => (
   <ServiceContainerProvider serviceContainer={serviceContainer}>
     <ErrorBoundary>
-      <ThemeProvider>
-        <StorageConnector>{children}</StorageConnector>
-      </ThemeProvider>
+      <ThemeProvider>{children}</ThemeProvider>
     </ErrorBoundary>
   </ServiceContainerProvider>
 );
