8273582ac12ca1c1a4ccd0c9903917ed08578851
Romez <Romez1990@gmail.com>
2023-08-18 06:29:35 +0300

    fixup! chore: setup linter

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -647,7 +647,6 @@ module.exports = webpackEnv => {
           cwd: paths.appPath,
           resolvePluginsRelativeTo: __dirname,
           baseConfig: {
-            extends: [require.resolve('eslint-config-react-app/base')],
             rules: {
               ...(!hasJsxRuntime && {
                 'react/react-in-jsx-scope': 'error',
